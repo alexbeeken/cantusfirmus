d2d981ac5d9e78f6cc328c7dae66ccafeea366ca
Alex Beeken <aeb242@gmail.com>
2015-03-07 21:10:20 +0000

    tests for default values of melody and tonic

--- lib/cantusfirmus.rb
+++ lib/cantusfirmus.rb
@@ -1,8 +1,11 @@
 class CantusFirmus
-  attr_reader(:melody_length, :finished)
+  attr_reader(:melody_length, :finished, :tonic)
   
-  def initialize(params = {})
+  def initialize(params = {:melody_length => 8, :tonic => 60})
     @melody_length = params[:melody_length]
+    @tonic = params[:tonic]
     @finished = false
   end
+ 
+  
 end
--- spec/cantusfirmus_spec.rb
+++ spec/cantusfirmus_spec.rb
@@ -3,8 +3,24 @@ require 'pry'
 
 describe 'initialize' do
   it 'creates a new blank array of length 8 and a finished value of false' do
-    test = CantusFirmus.new({:melody_length => 8})
+    test = CantusFirmus.new({:melody_length => 8, :tonic => 60})
     expect(test.melody_length).to eq(8)
     expect(test.finished).to eq(false)
+    expect(test.tonic).to eq(60)
+  end
+  
+  it 'defaults to melody length 8 and tonic 60 if not specified' do
+    test = CantusFirmus.new()
+    expect(test.melody_length).to eq(8)
+    expect(test.finished).to eq(false)
+    expect(test.tonic).to eq(60)
+    
+    test2 = CantusFirmus.new({:melody_length => 10})
+    expect(test.melody_length).to eq(10)
+    expect(test.tonic).to eq(60)
+    
+    test3 = CantusFirmus.new({:tonic => 55})
+    expect(test.melody_length).to eq(8)
+    expect(test.tonic).to eq(55)
   end
 end
