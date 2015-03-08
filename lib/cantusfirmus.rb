d48f822ef82f3f7492ad5646af74f1aa2fcc2d02
Alex Beeken <student@epicodus.com>
2015-03-08 14:34:19 -0700

    refactor default values in cantusfirmus initialize

--- lib/cantusfirmus.rb
+++ lib/cantusfirmus.rb
@@ -1,21 +1,15 @@
 class CantusFirmus
   attr_reader(:melody_length, :finished, :tonic, :notes)
-  
+
   def initialize(params ={})
-      @melody_length = params[:melody_length]
-      @tonic = params[:tonic]
+      @melody_length = params.fetch(:melody_length, 8)
+      @tonic = params.fetch(:tonic, 60)
       @finished = false
-    if @melody_length == nil
-      @melody_length = 8
-    end
-    if @tonic == nil
-      @tonic = 60
-    end
       @notes = [@tonic]
   end
-  
+
   def add_note
     @notes
   end
-  
+
 end
