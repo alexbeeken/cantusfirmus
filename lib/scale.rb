b417dadd33a561669750526a2617493cace517f6
Alex Beeken <aeb242@gmail.com>
2015-03-12 18:51:38 -0700

    Have Scale Class, Pass All Specs

--- lib/scale.rb
+++ lib/scale.rb
@@ -3,19 +3,60 @@ class Scale
 
   def initialize
     @mode = "major"
-    @diatonic = [-15, -13, -12, -10, -8, -7, -5, -3, -1, 2, 4, 5, 7, 9, 11, 12, 14, 16]
-    @consonant_intervals = [ -15, -14, -13, -12, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16]
+    @diatonic = [-15, -13, -12, -10, -8, -7, -5, -3, -1, 0, 2, 4, 5, 7, 9, 11, 12, 14, 16]
   end
 
-  def dissonances(note)
+  def get_notes_for_relationship(params = {})
     output = []
-    @diatonic.each do |diatonic_note|
-      interval = 0 - (note - diatonic_note)
-      if !(@consonant_intervals.include?(interval))
+    note = params.fetch(:note, 0)
+    relationship = params.fetch(:relationship, 'dissonant')
+    direction = params.fetch(:direction, nil)
+    output = check_notes(note, relationship)
+    return output
+  end
+
+  private
+
+  def check_notes(note, relationship)
+    output = []
+    diatonic = [-15, -13, -12, -10, -8, -7, -5, -3, -1, 0, 2, 4, 5, 7, 9, 11, 12, 14, 16]
+    diatonic.each do |diatonic_note|
+      if relationship?(note, diatonic_note, relationship)
         output.push(diatonic_note)
       end
     end
     return output
+  end
+
+  def dissonance?(note1, note2)
+    consonant_intervals = [-15, -14, -13, -12, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16]
+    interval = (0 - get_interval(note1, note2))
+    return !(consonant_intervals.include?(interval))
+  end
+
+  def get_interval(note1, note2)
+    return (note1 - note2)
+  end
+
+  def leap?(note1, note2)
+    return (get_interval(note1, note2).abs > 4)
+  end
+
+  def relationship?(note1, note2, relationship)
+    return dissonance?(note1, note2) if relationship == 'dissonant'
+    return leap?(note1, note2) if relationship == 'leap'
+    return !(leap?(note1, note2)) if relationship == 'step'
+    return up?(note1, note2) if relationship == 'up'
+    return down?(note1, note2) if relationship == 'down'
+    return false
+  end
+
+  def up?(note1, note2)
+    return ((note1 - note2) <= -1)
+  end
+
+  def down?(note1, note2)
+    return ((note1 - note2) >= 1)
   end
 
 end
--- spec/scale_spec.rb
+++ spec/scale_spec.rb
@@ -7,9 +7,29 @@ describe 'Scale class' do
     @test_scale = Scale.new()
   end
 
-  describe '#dissonances' do
-    it 'returns only the dissonant intervals for a given note' do
-      expect(@test_scale.dissonances(0)).to eq([-10, 11])
+  describe '#get_notes_for_relationship' do
+    it 'returns notes based on the given relationship to given note with defaults 0 and dissonant' do
+      expect(@test_scale.get_notes_for_relationship()).to eq([-10, 11])
+    end
+
+    it 'returns leaps when given the leaps relationship' do
+      expect(@test_scale.get_notes_for_relationship({:relationship => 'leap'})).to eq([-15, -13, -12, -10, -8, -7, -5, 5, 7, 9, 11, 12, 14, 16])
+    end
+
+    it 'returns steps when given the steps relationship' do
+      expect(@test_scale.get_notes_for_relationship({:relationship => 'step'})).to eq([-3, -1, 0, 2, 4])
+    end
+
+    it 'returns steps from a given note when given a note and relationship' do
+      expect(@test_scale.get_notes_for_relationship({:relationship => 'step', :note => 2})).to eq([-1, 0, 2, 4, 5])
+    end
+
+    it 'returns notes above the given note' do
+      expect(@test_scale.get_notes_for_relationship({:relationship => 'up', :note => 12})).to eq([14, 16])
+    end
+
+    it 'returns notes below the given note' do
+      expect(@test_scale.get_notes_for_relationship({:relationship => 'down', :note => -10})).to eq([-15, -13, -12])
     end
   end
 end
