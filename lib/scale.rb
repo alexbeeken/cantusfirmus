9047bf6f405ef613d156395759c74f005fc8b2d4
Alex Beeken <aeb242@gmail.com>
2015-03-12 17:14:24 -0700

    Pass All But One Test

--- lib/scale.rb
+++ lib/scale.rb
@@ -1,7 +1,7 @@
 class Scale
   attr_reader(:diatonic)
 
-  def intialize
+  def initialize
     @mode = "major"
     @diatonic = [-15, -13, -12, -10, -8, -7, -5, -3, -1, 2, 4, 5, 7, 9, 11, 12, 14, 16]
     @consonant_intervals = [ -15, -14, -13, -12, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 14, 15, 16]
@@ -10,8 +10,8 @@ class Scale
   def dissonances(note)
     output = []
     @diatonic.each do |diatonic_note|
-      interval = note - diatonic_note
-      if (consonant_intervals.include?(interval))
+      interval = 0 - (note - diatonic_note)
+      if !(@consonant_intervals.include?(interval))
         output.push(diatonic_note)
       end
     end
--- spec/scale_spec.rb
+++ spec/scale_spec.rb
@@ -9,7 +9,7 @@ describe 'Scale class' do
 
   describe '#dissonances' do
     it 'returns only the dissonant intervals for a given note' do
-      expect(@test_scale.dissonances(0)).to eq(-10, 0, 11)
+      expect(@test_scale.dissonances(0)).to eq([-10, 11])
     end
   end
 end
