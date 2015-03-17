4de703c5a527a6b47e2d8f1a72cd78dc43372b78
Alex Beeken <aeb242@gmail.com>
2015-03-16 17:26:10 -0700

    Pass 1 Spec For Evaluator Class

--- lib/evaluator.rb
+++ lib/evaluator.rb
@@ -1,17 +1,19 @@
 class Evaluator
 
   def self.get_statistics(phrase)
-    @phrase = phrase
-    number_of_steps = find_number_of_steps
+    @phrase = phrase.notes
+    @length = phrase.notes.length
+    number_of_steps = find_number_of_steps()
+    return {:steps => number_of_steps}
   end
 
   private
 
-  def find_number_of_steps
+  def self.find_number_of_steps
     last_note = nil
     count = 0
-    for index in 0..(@phrase.length - 1)
-      if (@phrase[index]) && (@phrase[index - 1])
+    for index in 0..(@length - 1)
+      if !(@phrase[index].nil?) && !(@phrase[index - 1].nil?)
         if (@phrase[index] - @phrase[index -1]).abs == 2
           count += 1
         end
