37cbe26df938165fcb8cbcb1f475fe946e11b1a4
Alex Beeken <aeb242@gmail.com>
2015-03-19 17:06:14 -0700

    Have wireframe version of notes class

--- lib/note.rb
+++ lib/note.rb
@@ -1,6 +1,20 @@
 class Note
 
-  def initialize
+  def initialize(params = {})
+    @number = params.fetch(:note, 0)
+    @score = nil
+  end
+
+  def valid?(note2, note3)
+    exception_checks && normal_checks
+  end
+
+  private
+
+  def exception_checks
+  end
+
+  def normal_checks
   end
 
 end
