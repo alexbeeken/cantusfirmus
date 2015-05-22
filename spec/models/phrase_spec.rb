31ff6dc8c8e59dbe475f88c7b13e25358ff37985
Alex Beeken <aeb242@gmail.com>
2015-05-22 09:20:04 -0700

    Test for Custom Length of Phrase Length

--- spec/models/phrase_spec.rb
+++ spec/models/phrase_spec.rb
@@ -9,5 +9,11 @@ describe Phrase do
         expect(phrase.notes.length).to eq(8)
         expect(phrase.notes.last.class).to eq(Fixnum)
     end
+
+    it "Creates a valid phrase of 10 notes if given no examples" do
+        phrase = Phrase.new(length: 10)
+        expect(phrase.notes.length).to eq(10)
+        expect(phrase.notes.last.class).to eq(Fixnum)
+    end
   end
 end
